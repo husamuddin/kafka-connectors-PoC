eadc14297082f37953d7fd5327f874cad6aa00c2
Husamuddin Hamad <husamuddin.hamad@hudl.com>
2025-02-26 19:31:07 +0400

    fix: remove a redundant condition

--- lib/py-seed/src/seed/main.py
+++ lib/py-seed/src/seed/main.py
@@ -25,10 +25,6 @@ def main():
     parser.add_argument('-t', '--type', choices=["mongodb", "docdb"], required=True, type=str)
     parser.add_argument('-c', '--count', help="number of records to create", default=200, required=True, type=int)
     args = parser.parse_args()
-
-    if args.type not in ["mongodb", "docdb"]:
-        raise Exception(f"--type {args.type} must be mongodb or docdb")
-
     run[args.type](args.count)
 
 
